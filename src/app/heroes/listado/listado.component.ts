1b866952b7e0c43c0e8c84509b22d192f17e4167
Mariano <marianotrojas8@gmail.com>
2022-07-02 20:41:13 -0300

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent {
+
+  heroes:string[] = ['Spiderman','Ironman','Thor','Hulk','Capitan America'];
+  heroeBorrado:string = '';
+
+  borrarHeroe(){
+    this.heroeBorrado = this.heroes.shift() || '';
+  }
+
+
+
+}
